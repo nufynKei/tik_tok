5893e7267411bd07a40e822f945a7a4129f85d06
kei <kei.kim@nufyn.com>
2023-07-26 22:31:32 +0900

    create interest button #day2
    -interest page의 button요소들을 밖으로 꺼내서 하나의 위젯 클래스로 새로 만듬
    -Gesturedetector를 통해 click요소를 만들어주고, isSelected를 통해서 클릭시 색을 바꾸게 함
    -container를 animatedcontainer로 만들어서, 클릭 효과에 애니메이션을 줌

---
+++ lib/widgets/interest_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:tik_tok/constants/sizes.dart';
+
+class InterestButton extends StatefulWidget {
+  const InterestButton({
+    Key? key,
+    required this.interest,
+  }) : super(key: key);
+
+  final String interest;
+
+  @override
+  State<InterestButton> createState() => _InterestButtonState();
+}
+
+class _InterestButtonState extends State<InterestButton> {
+  bool _isSelected = false;
+
+  void _onTap() {
+    setState(() {
+      _isSelected = !_isSelected;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size14, horizontal: Sizes.size16),
+        decoration: BoxDecoration(
+            color: _isSelected ? Theme.of(context).primaryColor : Colors.white,
+            border: Border.all(color: Colors.black.withOpacity(0.1)),
+            borderRadius: BorderRadius.circular(Sizes.size32),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black.withOpacity(0.05),
+                  blurRadius: 5,
+                  spreadRadius: 5)
+            ]),
+        child: Text(
+          widget.interest,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
