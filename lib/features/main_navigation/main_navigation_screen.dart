f4875b974f5ded52e29dcb0751020923752762f7
kei <kei.kim@nufyn.com>
2023-07-27 15:20:32 +0900

    modify navigation screen #day3
    state를 가지는 화면을 body에 가져와서, selct icon의 index에 맞춰서 화면이 렌더링 되게 만듬.
    여기서 문제점은 state가 계속해서 rebuild 되는 것.

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tik_tok/constants/sizes.dart';
+import 'package:tik_tok/features/main_navigation/stf_screen.dart';
+import 'package:tik_tok/widgets/nav_tab.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({Key? key}) : super(key: key);
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final screens = [
+    const Center(
+      child: Text('Home'),
+    ),
+    const Center(
+      child: Text('Search'),
+    ),
+    const Center(
+      child: Text('Home'),
+    ),
+    const Center(
+      child: Text('Search'),
+    ),
+    const Center(
+      child: Text('Search'),
+    )
+  ];
+  final screens2 = [
+    StfScreen(
+      key: GlobalKey(),
+    ),
+    StfScreen(key: GlobalKey()),
+    Container(),
+    StfScreen(key: GlobalKey()),
+    StfScreen(key: GlobalKey()),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: screens2[_selectedIndex],
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.size12),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            NavTab(
+              onTap: () => _onTap(0),
+              text: "Home",
+              isSelect: _selectedIndex == 0,
+              icon: FontAwesomeIcons.house,
+              selectIcon: FontAwesomeIcons.houseChimney,
+            ),
+            NavTab(
+              onTap: () => _onTap(1),
+              text: "Discover",
+              isSelect: _selectedIndex == 1,
+              icon: FontAwesomeIcons.compass,
+              selectIcon: FontAwesomeIcons.solidCompass,
+            ),
+            NavTab(
+              onTap: () => _onTap(3),
+              text: "Inbox",
+              isSelect: _selectedIndex == 3,
+              icon: FontAwesomeIcons.message,
+              selectIcon: FontAwesomeIcons.solidMessage,
+            ),
+            NavTab(
+              onTap: () => _onTap(4),
+              text: "Profile",
+              isSelect: _selectedIndex == 4,
+              icon: FontAwesomeIcons.user,
+              selectIcon: FontAwesomeIcons.solidUser,
+            )
+          ]),
+        ),
+      ),
+    );
   }
 }
