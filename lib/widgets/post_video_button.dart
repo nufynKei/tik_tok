2050d6716d61a0f37605fb5ffc531a743964535b
kei <kei.kim@nufyn.com>
2023-07-27 22:03:06 +0900

    create video button #day3
    만들어둔 video button을 따로 위젯으로 뺌

---
+++ lib/widgets/post_video_button.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tik_tok/constants/sizes.dart';
+
+class PostVideoButton extends StatelessWidget {
+  const PostVideoButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Positioned(
+          right: Sizes.size16 + Sizes.size8,
+          child: Container(
+            height: Sizes.size28 + Sizes.size2,
+            width: Sizes.size24,
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size8),
+            decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(Sizes.size11)),
+          ),
+        ),
+        Positioned(
+          left: Sizes.size16 + Sizes.size8,
+          child: Container(
+            height: Sizes.size28 + Sizes.size2,
+            width: Sizes.size24,
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size8),
+            decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(Sizes.size11)),
+          ),
+        ),
+        Container(
+          height: Sizes.size28 + Sizes.size2,
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size12),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(Sizes.size10)),
+          child: const Center(
+              child: FaIcon(
+            FontAwesomeIcons.plus,
+            size: Sizes.size20,
+            color: Colors.black,
+          )),
+        )
+      ],
+    );
+  }
+}
