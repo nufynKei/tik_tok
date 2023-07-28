7dde993a2ce9a8597a83dab27513e9a2d4ba2d50
kei <kei.kim@nufyn.com>
2023-07-28 10:15:11 +0900

    자동 생성 파일 #day4

--- lib/features/video/video_timeline_screen.dart
+++ lib/features/video/video_timeline_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tik_tok/widgets/video_post.dart';
 
 class VideoTimaline extends StatefulWidget {
   const VideoTimaline({Key? key}) : super(key: key);
@@ -8,16 +9,64 @@ class VideoTimaline extends StatefulWidget {
 }
 
 class _VideoTimalineState extends State<VideoTimaline> {
+  
+  int _itemCount = 4;
+  final PageController _pageController = PageController();
+  final _scrollDuration = const Duration(milliseconds: 200);
+  final _scrollCurve = Curves.decelerate;
+
+  List<Color> colors = [
+    Colors.blue,
+    Colors.red,
+    Colors.yellow,
+    Colors.teal,
+  ];
+
+  ///page의 index번호와 _itemCount 숫자를 비교
+  ///만약 page가 3번째 이면, itemcount - 1 과 숫자가 같아지니 itemcount는 다시 늘어나고,
+  ///그것에 맞춰서, addAll 함수를 이용해, color 갯수도 늘린다.
+  ///단지 이것은 무한스크롤이 가능하게 만들기 위해서 이다.
+  // void _onPageChanged(int page) {
+  //   if (page == _itemCount - 1) {
+  //     _itemCount = _itemCount + 4;
+  //     colors.addAll([
+  //       Colors.blue,
+  //       Colors.red,
+  //       Colors.yellow,
+  //       Colors.teal,
+  //     ]);
+  //     setState(() {});
+  //   }
+  // }
+  void _onPageChanged(int page) {
+    _pageController.animateToPage(page,
+        duration: _scrollDuration, curve: _scrollCurve);
+    if (page == _itemCount - 1) {
+      _itemCount = _itemCount + 4;
+      colors.addAll([
+        Colors.blue,
+        Colors.red,
+        Colors.yellow,
+        Colors.teal,
+      ]);
+    }
+    setState(() {});
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(duration: _scrollDuration, curve: _scrollCurve);
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      children: [
-        Container(color: Colors.blue),
-        Container(color: Colors.teal),
-        Container(color: Colors.yellow),
-        Container(color: Colors.green),
-        Container(color: Colors.red),
-      ],
+    return PageView.builder(
+        controller: _pageController,
+        scrollDirection: Axis.vertical,
+        onPageChanged: _onPageChanged,
+        itemCount: _itemCount,
+        itemBuilder: (context, index) => VideoPost(
+              onVideoFinished: _onVideoFinished,
+            )
     );
   }
 }
