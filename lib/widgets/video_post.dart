21da1c765c027f7a1d3ea57b6b3363aebd629178
kei <kei.kim@nufyn.com>
2023-07-28 15:37:01 +0900

    modify video post #day4
    addlistner 와 setState 대신 AnimatedBuilder를 이용해서 위젯을 통해 값의 변화를 관찰하고, build 함

--- lib/widgets/video_post.dart
+++ lib/widgets/video_post.dart
@@ -53,9 +53,6 @@ class _VideoPostState extends State<VideoPost>
         upperBound: 1.5,
         value: 1.5,
         duration: _animationDuration);
-    _animationController.addListener(() {
-      setState(() {});
-    });
   }
 
   @override
@@ -106,8 +103,16 @@ class _VideoPostState extends State<VideoPost>
           Positioned.fill(
             child: IgnorePointer(
               child: Center(
-                child: Transform.scale(
-                  scale: _animationController.value,
+                child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (context, child) {
+                    return
+                        //animationController의 값이 변할 때마다 실행
+                        Transform.scale(
+                      scale: _animationController.value,
+                      child: child,
+                    );
+                  },
                   child: AnimatedOpacity(
                     opacity: isPaused ? 1 : 0,
                     duration: _animationDuration,
