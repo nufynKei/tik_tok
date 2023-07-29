3727a4d2a53c2a1c6811545403e8bac7a0e941ed
kei <kei.kim@nufyn.com>
2023-07-29 22:17:57 +0900

    modify video icon button #day5
    아이콘 버튼 component에 GestureDetector 위젯 추가해줌.
    Function? nullable하게 만들었기 때문에, onTap?.call() 을 해줌. null이 아닐 경우 함수를 호출하는 것임

--- lib/widgets/video_icon_button.dart
+++ lib/widgets/video_icon_button.dart
@@ -4,29 +4,34 @@ import 'package:tik_tok/constants/gaps.dart';
 import 'package:tik_tok/constants/sizes.dart';
 
 class VideoIconButton extends StatelessWidget {
-  const VideoIconButton({Key? key, required this.icon, required this.text})
+  const VideoIconButton(
+      {Key? key, required this.icon, required this.text, this.onTap})
       : super(key: key);
 
   final IconData icon;
   final String text;
+  final Function? onTap;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        FaIcon(
-          icon,
-          color: Colors.white,
-          size: Sizes.size36,
-        ),
-        Gaps.v6,
-        Text(
-          text,
-          style: const TextStyle(
+    return GestureDetector(
+      onTap: () => onTap?.call(),
+      child: Column(
+        children: [
+          FaIcon(
+            icon,
             color: Colors.white,
-            fontWeight: FontWeight.bold,
+            size: Sizes.size36,
           ),
-        ),
-      ],
+          Gaps.v6,
+          Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
