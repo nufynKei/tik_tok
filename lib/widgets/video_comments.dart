337fb4d6a867b7636e0fbb123110aa5bb8c09ad8
kei <kei.kim@nufyn.com>
2023-07-30 01:04:45 +0900

    modify comments #day5
    listview.Builder 에서 listView.separated로 바꿔줌.
    
    Row 아이템 마다 간격이 필요하기 때문에, separatorBuilder 로 Gaps를 줌

--- lib/widgets/video_comments.dart
+++ lib/widgets/video_comments.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tik_tok/constants/gaps.dart';
+import 'package:tik_tok/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
   const VideoComments({Key? key}) : super(key: key);
@@ -26,7 +28,9 @@ class _VideoCommentsState extends State<VideoComments> {
     return SizedBox(
       height: screenHeight * 0.6,
       child: Scaffold(
+        backgroundColor: Colors.grey.shade50,
         appBar: AppBar(
+          backgroundColor: Colors.grey.shade50,
           //AbbBar에 자동적으로 나오는 backButton icon 기능 끄기
           automaticallyImplyLeading: false,
           title: const Text("123123 comments"),
@@ -35,8 +39,72 @@ class _VideoCommentsState extends State<VideoComments> {
                 onPressed: _onBack, icon: const FaIcon(FontAwesomeIcons.xmark))
           ],
         ),
-        body: Container(),
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
+          separatorBuilder: (context, index) => Gaps.v20,
+          itemCount: 10,
+          itemBuilder: (context, index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                radius: 18,
+                child: Text("니꼬"),
+              ),
+              Gaps.h10,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '니꼬',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: Sizes.size14,
+                          color: Colors.grey.shade500),
+                    ),
+                    Gaps.v3,
+                    const Text(
+                        "That's not it l've seen the same thing but also in a cave,")
+                  ],
+                ),
+              ),
+              Gaps.h10,
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.heart,
+                    size: Sizes.size20,
+                    color: Colors.grey.shade500,
+                  ),
+                  Gaps.v2,
+                  Text(
+                    '52.2K',
+                    style: TextStyle(
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                backgroundColor: Colors.grey.shade500,
+                foregroundColor: Colors.white,
+                child: const Text("니꼬"),
+              ),
+            ],
+          ),
+        ),
       ),
-    );
+    ); 
   }
 }
